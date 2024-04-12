796394019ad35e6f9a6317d2c89010e39ecef400
myvsgit <qlwyb@aoyagi-cq.com>
2024-04-12 09:40:28 +0800

    TBNUVA05_DEMO6.bin

--- PRJ/PRJ/poweroff.h
+++ PRJ/PRJ/poweroff.h
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 
 #define VOLT_SLEEP  (0x99F-(TYPE_PRODUCT?0:1)*0x1CD)//(0xC31-(READ_PRODUCTTYPE?0:1)*0x200)//6.6V  B7C-6.2V//8V
-#define VOLT_WORK   (0x99f-(TYPE_PRODUCT?0:1)*0x1CD)//A39-8.5V 99F-8V 0xAD3-9V
+#define VOLT_WORK   (0x86C-(TYPE_PRODUCT?0:1)*0xE1)//A39-8.5V 99F-8V 0xAD3-9V
 
 void main_pwr_process(void);
 void LVD_init_scan(void);
